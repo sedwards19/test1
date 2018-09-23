ad04fb0b7aded2b1d9253d9da3723d8a6f64e952
Sydney Edwards <sydneyedwards@sydneys-air.attlocal.net>
2018-09-23 16:48:01 -0400

    Undefined branch & log

---
+++ main.js
@@ -0,0 +1,22 @@
+var myJavaScriptPractice = "This is my JavaScript Practice";
+console.log(myJavaScriptPractice);
+
+var num = 5;
+console.log(num);
+
+var numAddition = (5+5);
+console.log(numAddition);
+
+var numConcatOne = 2;
+var numConcatTwo = "3";
+console.log(numConcatOne+numConcatTwo);
+
+var x = 7;
+var y = 8;
+console.log(x>y);
+
+var empty = null;
+console.log(empty);
+
+var noValue;
+console.log(noValue);
